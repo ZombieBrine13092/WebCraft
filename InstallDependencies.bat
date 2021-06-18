2eac7babeaf02bbfc4bf1f504752fea2c2791994
Keagan <keagan.bowser13092@gmail.com>
2021-06-17 20:59:04 -0400

    Updated dependencies installer
    InstallDependencies.bat now installs the required dependencies if they aren't found and runs the game after they are.

--- InstallDependencies.bat
+++ InstallDependencies.bat
@@ -1,1 +1,7 @@
 npm install -g http-server&npm install socket.io&npm install express
+echo Dependencies were installed. You can now run WebCraft.
+echo Press any key to exit and run the game, or close the window to close without running it.
+pause>nul
+start StartServer.bat
+start JoinServer.bat
+exit
